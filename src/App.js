27d691bce199e0d8b39edee10861a76a9f96e347
Your Name <you@example.com>
2024-06-17 09:47:27 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { HomePage, AdminPage } from "./Pages";
+import { Register, Login, MovieList, AddScreen, Navbar } from "./components";
+
+const theme = createTheme();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/admin" element={<AdminPage />} />
+          <Route path="/admin/add-screen" element={<AddScreen />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/movies" element={<MovieList />} />
+          {/* <Route path="/seat-selection" element={<SeatSelection />} /> */}
+          {/* Add more routes as needed */}
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
